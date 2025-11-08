7521cb8e2104ddd876da941b98f50792014c78dc
SlickYeet <lasse@famlam.ca>
2025-11-08 02:00:13 -0800

    feat: update turbo.json tasks and add ESLint and Prettier configurations

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
+ */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  proseWrap: "always",
+}
+
+export default config
